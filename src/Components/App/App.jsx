f3fedd6db472e96fe48f7154217cba76626277f4
farshad <far5had@outlook.com>
2023-05-09 19:16:37 +0200

    debug signup page

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,12 +1,13 @@
 import './App.scss';
 import { SignUp } from '../Auth/SignUp/SignUp';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 function App() {
   
   return (
     <>
       <h1>Welcome</h1>
+      <h2>Pocket Counter</h2>
       <SignUp />
     </>
   );
