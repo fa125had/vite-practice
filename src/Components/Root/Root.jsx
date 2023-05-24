fa6a65b0ae423a03f332d25e88c7ba2d903e06a0
farshad <far5had@outlook.com>
2023-05-24 18:56:20 +0200

    changed style mode to object module

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom'
 
 export const Root = () => {
   return (
     <div>
       <h1>Pocket Counter</h1>
-      <h2>Welcome, Please Login</h2>
+      <h2>Welcome</h2>
       <Outlet />
     </div>
   )
