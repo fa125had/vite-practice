cc0f18b49d5e8b13c0e4d4d8c5b2c95e75d14161
farshad <far5had@outlook.com>
2023-05-24 20:17:54 +0200

    root-page bug fix

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,11 +1,24 @@
 import { Outlet, Link } from 'react-router-dom'
+import {useState} from 'react'
 
 export const Root = () => {
+  const [loginPrompt, setLoginPrompt] = useState(true)
+
+  const handleLoginClick = () => {
+    setLoginPrompt(false);
+  }
+
   return (
     <div>
       <h1>Pocket Counter</h1>
       <h2>Welcome</h2>
-      <p>Please<Link to='/login'> Login </Link>to your account!</p>
+      {loginPrompt && 
+      <>
+        <p>Please<Link to='/login' onClick={handleLoginClick}> Login </Link>to your account!</p>
+        <p>Or if you don&apos;t have one,<Link to='/register' onClick={handleLoginClick}> Create one</Link>!</p>
+      </>
+        
+          }
       <Outlet />
     </div>
   )
